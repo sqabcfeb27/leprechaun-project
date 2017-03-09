91be3adb6ca7cb50936b110840895089aa6d45b8
Jean-francois Nepton <sqasolution@hotmail.com>
2017-03-08 19:53:20 -0800

    Update AppBasics With More request* Methods

--- src/main/java/com/sqa/jf/helpers/AppBasics.java
+++ src/main/java/com/sqa/jf/helpers/AppBasics.java
@@ -24,31 +24,11 @@ public class AppBasics {
 
 	static Scanner scanner = new Scanner(System.in);
 
-	public static boolean requestBoolean(String question) {
-		String input;
-		boolean booleanValue = false;
-		System.out.print(question + " ");
-		input = scanner.nextLine();
-		try {
-			booleanValue = Boolean.parseBoolean(input.trim());
-		} catch (NumberFormatException e) {
-			System.out
-					.println("You have not entered a correct boolean value (" + input + ")\nUsing false for set value");
-		}
-		return booleanValue;
-	}
-
-	public static char requestChar(String question) {
-		String input;
-		char character = 'A';
-		System.out.print(question + " ");
-		input = scanner.nextLine();
-		if (input.trim().length() > 1) {
-			System.out.println("You have not entered more than one character (" + input + ")\nUsing 'A' for set value");
-		} else {
-			character = input.trim().charAt(0);
-		}
-		return character;
+	/**
+	 * Helper method to farewell user given the appName and user name.
+	 */
+	public static void farewellUser(String appName, String user) {
+		System.out.println("Thank you for using the " + appName + " app " + user + ", have a great day!");
 	}
 
 	/**
@@ -62,71 +42,19 @@ public class AppBasics {
 		System.out.print(question + " ");
 		input = scanner.nextLine();
 		try {
-			count = Double.parseDouble(input.trim());
+			count = Integer.parseInt(input.trim());
 		} catch (NumberFormatException e) {
 			System.out.println("You have not entered a correct numerical value (" + input + ")\nUsing 0 for set value");
 		}
 		return count;
 	}
 
-	public static float requestFloat(String question) {
-		String input;
-		float count = 0;
-		System.out.print(question + " ");
-		input = scanner.nextLine();
-		try {
-			count = Float.parseFloat(input.trim());
-		} catch (NumberFormatException e) {
-			System.out.println(
-					"You have not entered a correct float numerical value (" + input + ")\nUsing 0 for set value");
-		}
-		return count;
-	}
-
-	public static int requestInt(String question) {
-		String input;
-		int count = 0;
-		System.out.print(question + " ");
-		input = scanner.nextLine();
-		try {
-			count = Integer.parseInt(input.trim());
-		} catch (NumberFormatException e) {
-			System.out.println(
-					"You have not entered a correct int numerical value (" + input + ")\nUsing 0 for set value");
-		}
-		return count;
-	}
-
-	public static long requestLong(String question) {
-		String input;
-		long count = 0;
-		System.out.print(question + " ");
-		input = scanner.nextLine();
-		try {
-			count = Long.parseLong(input.trim());
-		} catch (NumberFormatException e) {
-			System.out.println(
-					"You have not entered a correct Long numerical value (" + input + ")\nUsing 0 for set value");
-		}
-		return count;
-	}
-
-	public static short requestShort(String question) {
-		String input;
-		short count = 0;
-		System.out.print(question + " ");
-		input = scanner.nextLine();
-		try {
-			count = Short.parseShort(input.trim());
-		} catch (NumberFormatException e) {
-			System.out.println(
-					"You have not entered a correct Short numerical value (" + input + ")\nUsing 0 for set value");
-		}
-		return count;
-	}
-
-	public static String requestString(String question) {
-		System.out.print(question + " ");
+	/**
+	 * Helper method to welcome user and return their supplied name.
+	 */
+	public static String welcomeUserAndGetName(String appName) {
+		System.out.println("Welcome to the " + appName + " application.\n");
+		System.out.print("Could I get your name? ");
 		return scanner.nextLine();
 	}
 }
